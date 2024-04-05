1263db7941de8dc241e3e0f1d919d6bf03b6914a
Jens <jens.coosemans@student.kuleuven.be>
2024-04-05 20:53:09 +0200

    adjusted train.py

--- train.py
+++ train.py
@@ -123,7 +123,11 @@ if __name__=='__main__':
     #model
     model = args.model
 
-    # best_all = np.zeros([args.k_fold_num, 4])
+    best_all = np.zeros([args.k_fold_num, 4])
+
+    ## for k_fold_id in range(1,args.k_fold_num + 1):
+
+
 
     #check gpu readiness
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -149,6 +153,113 @@ if __name__=='__main__':
     transformations_test = transforms.Compose([transforms.CenterCrop(224),transforms.ToTensor(),\
                 transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
     
+    print('Training set: ' + train_filename_list)
+
+    #load the multi modal network 
+    ###needs to be implemented
+    if model == 'MM_NSSI':
+        model = MM_NSSInet()
+        model = model.to(device)
+        print('Using model: MM-PCQA')
+
+    #define the loss function
+    if loss == 'l2rank':
+        criterion : L2RankLoss().to(device)
+        print('Using l2rank loss')
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay= decay_rate)
+    print('Using Adam optimizer, initial learning rate: ' + str(args.learning_rate))
+
+    #let the optimize adjust its learning rate every 8 epochs
+    scheduler  = torch.optim.lr_scheduler.StepLR(optimizer, step_size=8, gamma=0.9)
+
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+    print("starting the training")
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+
+    best_test_criterion = -1  # SROCC min
+    best = np.zeros(4)
+
+    ###needs to be changed to the new 
+    train_dataset = MMDataset(data_dir_2d = data_dir_2d, data_dir_nss = data_dir_nss, datainfo_path = train_filename_list, transform = transformations_train)
+    test_dataset = MMDataset(data_dir_2d = data_dir_2d, data_dir_pc = data_dir_nss, datainfo_path = test_filename_list, transform = transformations_test, is_train = False)
+    
+    for epoch in range(num_epochs):
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+        test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=1  , shuffle=False, num_workers=8)
+
+        n_train = len(train_dataset)
+        n_test = len(test_dataset)
+
+        model.train()
+        start = time.time()
+        batch_losses = []
+        batch_losses_each_disp = []
+
+        x_output = np.zeros(n_train)
+        x_test = np.zeros(n_train)
+
+        for i, (imgs, nss, mos) in enumerate(train_loader):
+            #get data from loader
+            imgs = imgs.to(device)
+            nss = nss.to(device)
+            mos = mos[:, np.newaxis]
+            mos = mos.to(device)
+            mos_output = model(imgs,nss)
+
+            #compute the loss, and reset the gradient afterwards (not done automatically)
+            loss = criterion(mos_output, mos)
+            batch_losses.append(loss)
+            batch_losses_each_disp.append(loss.item())
+            optimizer.zero_grad()           #clear for next training
+            torch.autograd.backward(loss)
+            optimizer.step()
+
+        avg_loss = sum(batch_losses) / (len(test_dataset) // batch_size)
+        print('Epoch %d averaged training loss: %.4f' % (epoch))
+
+        scheduler.step()
+        lr_current = scheduler.get_last_lr()
+        print('The current learning rate is {:.06f}'.format(lr_current[0]))
+
+        end = time.time()
+        print('Epoch %d training time cost: %.4f seconds' % (epoch + 1, end-start))
+
+        # Test 
+        model.eval()
+        y_output = np.zeros(n_test)
+        y_test = np.zeros(n_test)
+
+        with torch.no_grad():
+            for i, (imgs, pc, mos) in enumerate(test_loader):
+                imgs = imgs.to(device)
+                pc = torch.Tensor(pc.float())
+                pc = pc.to(device)
+                y_test[i] = mos.item()
+                outputs = model(imgs, pc)
+                y_output[i] = outputs.item()
+
+
+            y_output_logistic = fit_function(y_test, y_output)
+            test_PLCC = stats.pearsonr(y_output_logistic, y_test)[0]
+            test_SROCC = stats.spearmanr(y_output, y_test)[0]
+            test_RMSE = np.sqrt(((y_output_logistic-y_test) ** 2).mean())
+            test_KROCC = scipy.stats.kendalltau(y_output, y_test)[0]
+            print("Test results: SROCC={:.4f}, KROCC={:.4f}, PLCC={:.4f}, RMSE={:.4f}".format(test_SROCC, test_KROCC, test_PLCC, test_RMSE))
+
+            if test_SROCC > best_test_criterion:
+                print("Update best model using best_val_criterion ")
+                #torch.save(model.state_dict(), 'ckpts/' + database + '_' + str(k_fold_id) + '_best_model.pth')
+                # scio.savemat(trained_model_file+'.mat',{'y_pred':y_pred,'y_test':y_test})
+                best[0:4] = [test_SROCC, test_KROCC, test_PLCC, test_RMSE]
+                best_test_criterion = test_SROCC  # update best val SROCC
+
+                print("Update the best Test results: SROCC={:.4f}, KROCC={:.4f}, PLCC={:.4f}, RMSE={:.4f}".format(test_SROCC, test_KROCC, test_PLCC, test_RMSE))
+    
+    print(database)
+    # best_all[k_fold_id-1, :] = best
+    print("The best Val results: SROCC={:.4f}, KROCC={:.4f}, PLCC={:.4f}, RMSE={:.4f}".format(best[0], best[1], best[2], best[3]))
+    print('*************************************************************************************************************************')
 
 
 
