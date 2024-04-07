51b980fd66712440a2a4a7597e60e6fb81289dad
Jens <jens.coosemans@student.kuleuven.be>
2024-04-07 23:14:07 +0200

    add script to calculate all projections

--- run_all_projections.sh
+++ run_all_projections.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-cd WPC
+target_pc_directory="WPC/point_clouds"
+target_image_directory="WPC/projections"
+for file in $(find "$target_pc_directory" -type f -name "*.ply" | sort)
+do 
+    base_file="$(basename $file)"
+    echo "$base_file"
+
+    target_projection_directory="${target_image_directory}/${base_file}"
+    echo "$target_projection_directory"
+
+    python utils/projections.py --pc_path "$file" \
+                             --image_path "$target_projection_directory" \
+                             --x_projections 4 \
+                             --y_projections 4
+
+done
