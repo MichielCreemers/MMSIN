cb452cc350bfee3ba4f437d26d022d0f1c8228b2
Jens <jens.coosemans@student.kuleuven.be>
2024-04-07 23:22:27 +0200

    update gitignore and start nightly extraction run

--- run_all_projections.sh
+++ run_all_projections.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 target_pc_directory="WPC/point_clouds"
-target_image_directory="WPC/projections"
+target_image_directory="WPC/projections_8"
 for file in $(find "$target_pc_directory" -type f -name "*.ply" | sort)
 do 
     base_file="$(basename $file)"
@@ -12,7 +12,23 @@ do
 
     python utils/projections.py --pc_path "$file" \
                              --image_path "$target_projection_directory" \
-                             --x_projections 4 \
-                             --y_projections 4
+                             --x_projections 8 \
+                             --y_projections 8
+
+done
+
+target_image_directory="WPC/projections_16"
+for file in $(find "$target_pc_directory" -type f -name "*.ply" | sort)
+do 
+    base_file="$(basename $file)"
+    echo "$base_file"
+
+    target_projection_directory="${target_image_directory}/${base_file}"
+    echo "$target_projection_directory"
+
+    python utils/projections.py --pc_path "$file" \
+                             --image_path "$target_projection_directory" \
+                             --x_projections 16 \
+                             --y_projections 16
 
 done
