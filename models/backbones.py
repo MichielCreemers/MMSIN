c979d99ad557310d99f49a6c48b0ec0c4d404d6a
Jens <jens.coosemans@student.kuleuven.be>
2024-04-09 23:36:12 +0200

    clean up backbones to only include needed resnet models

--- models/backbones.py
+++ models/backbones.py
@@ -16,15 +16,8 @@ __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'wide_resnet50_2', 'wide_resnet101_2']
 
 model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
     'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
     'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-    'resnext50_32x4d': 'https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth',
-    'resnext101_32x8d': 'https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth',
-    'wide_resnet50_2': 'https://download.pytorch.org/models/wide_resnet50_2-95faca4d.pth',
-    'wide_resnet101_2': 'https://download.pytorch.org/models/wide_resnet101_2-32ee1156.pth',
 }
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
@@ -85,45 +78,63 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     """
     More efficient residual block (block that does not only feed input to the next layer, but also to  2-3 layers further) 
-    than the BasicBlock (renset 18 and 34) class and is used in Resnet50 and higher. 
+    than the BasicBlock (renset 18 and 34) class and is used in Resnet50 and higher. it works by first reducing the dimension, and later expaning it again
     """
+    #output channels are 4 times input channels
     expansion = 4
     __constants__ = ['downsample']
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
                  base_width=64, dilation=1, norm_layer=None):
+        """
+            inplanes: input channels
+            planes: output channels
+            stride: step size at which the convolution happens
+            downsample: used to downsample the input in case the dimensions don't match 
+        """
         super(Bottleneck, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         width = int(planes * (base_width / 64.)) * groups
+
         # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        # define convolutional end batch normalization layers
+        #reduce the dimensions by convoltion with 1x1 3x3 and 1x1 again.
         self.conv1 = conv1x1(inplanes, width)
         self.bn1 = norm_layer(width)
         self.conv2 = conv3x3(width, width, stride, groups, dilation)
         self.bn2 = norm_layer(width)
         self.conv3 = conv1x1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
+
+        #add Rectified Liniar Activation function
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
 
     def forward(self, x):
+        """
+        Apply the concolutional layers, batch normalisation and relu activation
+        x: input tensor
+        """
         identity = x
 
+        #layer1
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
 
+        #layer2
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
 
+        #layer3, don't add relu to rpeserve residual connection (which is obtained by the addition)
         out = self.conv3(out)
         out = self.bn3(out)
-
         if self.downsample is not None:
             identity = self.downsample(x)
-
+        
         out += identity
         out = self.relu(out)
 
@@ -131,10 +142,25 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
+    """
+    Residual Network, resolves the vanishing gradient problem in deep learning networks: 
+        gradients become very small during backpropagation
+    Residual connections: backpropagate directly to earlier layers
 
+    """
     def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
                  norm_layer=None):
+        """
+        intialisation of the network
+        block:          type of residual block used
+        layers:         number of blocks in each layer
+        num_classes:    number of output classes
+        zero_init_resisual: initialize bn with 0
+        groups:         number of groups
+        width_per_group width per group for convolutional layers
+
+        """
         super(ResNet, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
@@ -151,11 +177,14 @@ class ResNet(nn.Module):
                              "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
+
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        #initialize the residual layers, which have a sequence of residual blocks
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
                                        dilate=replace_stride_with_dilation[0])
