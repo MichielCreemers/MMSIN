765808ee30d35eefd4cf782a212f4b2c62e91a56
Michiel <creemers.michiel@icloud.com>
2024-04-11 23:36:14 +0200

    Added argparse + config.json for train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import time
 
+import json
 import random
 import scipy
 from scipy import stats
@@ -12,6 +13,11 @@ import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torchvision import transforms
+
+from models.main_model import MM_NSSInet
+from utils.MultiModalDataset import MultiModalDataset
+from utils.loss import L2RankLoss
+
 
 def set_random_seed(seed=2024):
     """This function sets the seed for random number generation to ensure reproducibility of the results.
@@ -48,3 +54,41 @@ def logistic_function(X, beta1, beta2, beta3, beta4):
     y_hat = beta2 + np.divide(beta1 - beta2, logistic_part)
     return y_hat
 
+def fit_logistic_model(y_true, y_predict):
+    """Fits a logistic model to the given data.
+
+    Args:
+        y_true (numpy array): The true output values.
+        y_predict (numpy array): The initial prediction values.
+    
+    Returns:
+        numpy array: The output values predicted by the fitted logistic model.
+    """
+    # Initial parameters guess: upper asymptote, lower asymptote, midpoint x-value, curve steepness
+    betas = [np.max(y_true), np.min(y_true), np.mean(y_predict), 0.5]
+    
+    # Fit the logistic function to the data using curve_fit
+    optimized_params, _ = curve_fit(logistic_function, y_predict, y_true, p0=betas, maxfev=100000000)
+    
+    # Calculate fitted logistic model values
+    fitted_values = logistic_function(y_predict, *optimized_params)
+    
+    return fitted_values
+
+def parse_args():
+    """Parse input arguments from JSON config file."""
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    args = argparse.Namespace(**config)
+    return args
+
+
+if __name__ == "__main__":
+   
+    args = parse_args()
+    gpu_id = args.gpu
+    datasets = args.datasets
+    print("Using GPU ID:", gpu_id)
+    print("The datasets used are: ", datasets)
+    
+    
