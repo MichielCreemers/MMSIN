f2d9364fb7d00361d7247117aea55428c24c8eb3
Jens <jens.coosemans@student.kuleuven.be>
2024-04-12 21:40:47 +0200

    correct alfabetic mos

--- data_handling/alfabetize.py
+++ data_handling/alfabetize.py
@@ -2,11 +2,11 @@ import pandas as pd
 import csv
 
 #read input file
-df = pd.read_csv("WPC_NSS.csv")
+df = pd.read_csv("WPC/WPC_MOS.csv")
 
-sorted_df = df.sort_values(by="name", ascending=True)  
+sorted_df = df.sort_values(by="Ply_name", ascending=True)  
 
 #set output fule
-sorted_df.to_csv("WPC/WPC_NSS.csv", index=False)
+sorted_df.to_csv("WPC/WPC_MOS.csv", index=False)
 
 print(sorted_df)
