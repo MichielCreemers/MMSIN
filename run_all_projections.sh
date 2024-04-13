5ade27c5ed93240843ccce9de82e8739d9ef76fd
Jens <jens.coosemans@student.kuleuven.be>
2024-04-13 18:12:19 +0200

    add correct projection

---
+++ run_all_projections.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+target_pc_directory="WPC/point_clouds"
+target_image_directory="WPC/projections_good"
+
+for file in $(find "$target_pc_directory" -type f -name "*.ply" | sort)
+do 
+    base_file="$(basename $file)"
+    echo "$base_file"
+
+    target_projection_directory="${target_image_directory}/${base_file}"
+    echo "$target_projection_directory"
+
+    python utils/projections.py --pc_path "$file" \
+                             --image_path "$target_projection_directory" \
+                             --x_projections 4 \
+                             --y_projections 4 \
+                             --point_size 2
+
+done
