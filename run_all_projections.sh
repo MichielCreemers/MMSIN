01c0a3edd5e3d14b1e3f4e5c3c3cc8a47902405c
Jens <jens.coosemans@student.kuleuven.be>
2024-04-21 12:28:21 +0200

    add more tests

--- run_all_projections.sh
+++ run_all_projections.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 target_pc_directory="WPC/point_clouds"
-target_image_directory="WPC/projections_good_8"
+target_image_directory="WPC/projections_8_total"
 
 for file in $(find "$target_pc_directory" -type f -name "*.ply" | sort)
 do 
@@ -13,29 +13,10 @@ do
 
     python utils/projections.py --pc_path "$file" \
                              --image_path "$target_projection_directory" \
-                             --x_projections 8 \
-                             --y_projections 8 \
+                             --x_projections 5 \
+                             --y_projections 3 \
                              --point_size 2
 
 done
 
 #!/bin/bash
-
-target_pc_directory="WPC/point_clouds"
-target_image_directory="WPC/projections_good_16"
-
-for file in $(find "$target_pc_directory" -type f -name "*.ply" | sort)
-do 
-    base_file="$(basename $file)"
-    echo "$base_file"
-
-    target_projection_directory="${target_image_directory}/${base_file}"
-    echo "$target_projection_directory"
-
-    python utils/projections.py --pc_path "$file" \
-                             --image_path "$target_projection_directory" \
-                             --x_projections 16 \
-                             --y_projections 16 \
-                             --point_size 2
-
-done
