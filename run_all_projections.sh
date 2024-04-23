0a3030a5ad034c8bc1048021b48a05364295511b
Jens <jens.coosemans@student.kuleuven.be>
2024-04-23 22:20:23 +0200

    start gui

--- run_all_projections.sh
+++ run_all_projections.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-target_pc_directory="WPC/point_clouds"
-target_image_directory="WPC/projections_8_total"
+target_pc_directory="SJTU/point_clouds"
+target_image_directory="SJTU/projections"
 
 for file in $(find "$target_pc_directory" -type f -name "*.ply" | sort)
 do 
@@ -13,8 +13,8 @@ do
 
     python utils/projections.py --pc_path "$file" \
                              --image_path "$target_projection_directory" \
-                             --x_projections 5 \
-                             --y_projections 3 \
+                             --x_projections 4 \
+                             --y_projections 4 \
                              --point_size 2
 
 done
